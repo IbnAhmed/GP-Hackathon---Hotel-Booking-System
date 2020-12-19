1f19553ff7123e19c90ba54a6ee2f601d437c1da
Muntaha Ibn Ahmed <muntaha.techlogicians@gmail.com>
2020-12-19 13:49:19 +0600

    [Migration] customers table created

---
+++ database/migrations/2020_12_19_071827_create_customers_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            // Key
+            $table->bigIncrements('id');
+            
+            $table->unsignedBigInteger('registered_by'); // person who register the curtomer
+            $table->foreign('registered_by')->references('id')->on('users');
+
+            // General Information
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone');
+            $table->string('email');
+
+            // Time
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cutomers');
+    }
+}
